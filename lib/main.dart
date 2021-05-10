a24ac71d3ffce424516ef49113f80cff82d6454b
FlutLab.io <>
2021-05-10 19:06:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,45 @@ class Inicio extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.only(top: 130, bottom: 10, right: 10, left: 10),
         decoration: BoxDecoration(color: Colors.indigo, image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/nava33/MisImagenes/main/vacunas-consejos-peru.jpg"), alignment: Alignment.topCenter)), // fin caja
+        child : Column(
+          children: <Widget> [
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               Column(
+                 children: <Widget> [
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                     child: RaisedButton(
+                       color: Colors.white,
+                       shape: new RoundedRectangleBorder(
+                         borderRadius: BorderRadius.circular(10.0)
+                       ),
+                       onPressed: (){
+                         Navigator.pushNamed(context,"/inicio");
+                       },
+                       child :SizedBox(
+                          width: 100,
+                          height:100,
+                          child: Center(
+                              child : Text (
+                                "INICIO",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.indigo,
+                                fontWeight: FontWeight.w900 ),
+                              ),
+                          ), //center
+
+                       ), // sizedBox
+                     ), // boton inicio
+                    
+                  ) // padding
+
+                 ], //widget []
+               ), //fin columna interna
+             ), // fin fin de fila 1
+
+          ], // fin de widget []
+        ), // columna
       ), // fin de container
     ); // fin de Scaffold
   } // fin de widget inicio
